3897c8e7adcf7a7814ef7d8a881ee9d96297613c
shinyuna <yunadev01@gmail.com>
2020-12-31 04:45:01 +0900

    useTabs

---
+++ useTabs/useTabs.js
@@ -0,0 +1,8 @@
+export const useTabs = (initialTab, allTabs) => {
+  if (!allTabs && Array.isArray(allTabs)) return
+  const [currentIndex, setCurrentIndex] = useState(initialTab)
+  return {
+    currentItem: allTabs[currentIndex],
+    changeItem: setCurrentIndex,
+  }
+}
